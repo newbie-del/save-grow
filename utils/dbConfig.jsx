c87e54d46dff6c6bd61a1e638510fd0461757c58
Sudo_root <123349950+newbie-del@users.noreply.github.com>
2026-08-04 10:00:13 +0530

    Modify dbConfig for neon API key integration
    Updated database connection string to include neon API key.

--- utils/dbConfig.jsx
+++ utils/dbConfig.jsx
@@ -2,6 +2,6 @@ import { neon } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
 import * as schema from "./schema";
 const sql = neon(
-  "postgresql://save-and-grow:npg_EMCSbhxH4X8I@ep-soft-band-a5tres0r.us-east-2.aws.neon.tech/save-and-grow?sslmode=require"
+  "neon-api-key"
 );
 export const db = drizzle(sql, { schema });
